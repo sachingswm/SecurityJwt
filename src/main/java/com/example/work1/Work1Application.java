32a745ff68c3ddc4cc06f878e17805232b49fd23
SachinGoswami2021 <sachin.goswami@extramarks.net>
2021-08-12 14:09:29 +0530

    12/08/2021

---
+++ src/main/java/com/example/work1/Work1Application.java
@@ -0,0 +1,13 @@
+package com.example.work1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Work1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Work1Application.class, args);
+    }
+
+}
